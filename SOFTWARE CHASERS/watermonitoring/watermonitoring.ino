b5f54c689c78ea065ea176217bd7b8db3bf87a34
cyientApp <sandeep.doodigani@gmail.com>
2019-05-30 10:53:27 +0530

    sample

---
+++ SOFTWARE CHASERS/watermonitoring/watermonitoring.ino
@@ -0,0 +1,141 @@
+#include <ESP8266WiFi.h>--------
+#include <PubSubClient.h>
+
+
+
+#define SensorPin A0            //pH meter Analog output to Arduino Analog Input 0
+#define Offset 0.00            //deviation compensate
+#define samplingInterval 20
+#define printInterval 800
+#define ArrayLenth  40    //times of collection
+int pHArray[ArrayLenth];   //Store the average value of the sensor feedback
+int pHArrayIndex = 0;
+//-------- Customise these values -----------
+const char* ssid = "GANESH PATNALA";
+const char* password = "ganeshpatnala";
+float Turbidity;
+
+#define ORG "51r0th"
+#define DEVICE_TYPE "nodemcu"
+#define DEVICE_ID "9494"
+#define TOKEN "9494175805"
+//-------- Customise the above values --------
+
+char server[] = ORG ".messaging.internetofthings.ibmcloud.com";
+char topic[] = "iot-2/evt/Data/fmt/json";
+char authMethod[] = "use-token-auth";
+char token[] = TOKEN;
+char clientId[] = "d:" ORG ":" DEVICE_TYPE ":" DEVICE_ID;
+
+WiFiClient wifiClient;
+PubSubClient client(server, 1883, wifiClient);
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.print(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+
+  Serial.print("WiFi connected, IP address: ");
+  Serial.println(WiFi.localIP());
+  pinMode(D1, INPUT);
+
+}
+void PublishData(float pHValue, float Turbidity) {
+  if (!!!client.connected()) {
+    Serial.print("Reconnecting client to ");
+    Serial.println(server);
+    while (!!!client.connect(clientId, authMethod, token)) {
+      Serial.print(".");
+      delay(500);
+    }
+    Serial.println();
+  }
+  String payload = "{\"d\":{\"pHValue\":";
+  payload += pHValue;
+  payload += "," "\"Turbidity\":";
+  payload += Turbidity;
+  payload += "}}";
+  Serial.print("Sending payload: ");
+  Serial.println(payload);
+
+  if (client.publish(topic, (char*) payload.c_str())) {
+    Serial.println("Publish ok");
+  } else {
+    Serial.println("Publish failed");
+  }
+
+}
+void loop() {
+  Turbidity = digitalRead(D1);
+  Serial.print("Turbidity value is");
+  Serial.println(Turbidity);
+  delay(1000);
+  static unsigned long samplingTime = millis();
+  static unsigned long printTime = millis();
+  static float pHValue, voltage;
+  if (millis() - samplingTime > samplingInterval)
+  {
+    pHArray[pHArrayIndex++] = analogRead(SensorPin);
+    if (pHArrayIndex == ArrayLenth)pHArrayIndex = 0;
+    voltage = avergearray(pHArray, ArrayLenth) * 5.0 / 1024;
+    pHValue = 3.5 * voltage + Offset;
+    samplingTime = millis();
+  }
+  if (millis() - printTime > printInterval)  //Every 800 milliseconds, print a numerical, convert the state of the LED indicator
+  {
+    Serial.print("Voltage:");
+    Serial.print(voltage, 2);
+    Serial.print("    pH value: ");
+    Serial.println(pHValue, 2);
+    printTime = millis();
+  }
+  PublishData(pHValue, Turbidity);
+  delay(5000);
+}
+
+double avergearray(int* arr, int number) {
+  int i;
+  int max, min;
+  double avg;
+  long amount = 0;
+  if (number <= 0) {
+    Serial.println("Error number for the array to avraging!/n");
+    return 0;
+  }
+  if (number < 5) { //less than 5, calculated directly statistics
+    for (i = 0; i < number; i++) {
+      amount += arr[i];
+    }
+    avg = amount / number;
+    return avg;
+  } else {
+    if (arr[0] < arr[1]) {
+      min = arr[0]; max = arr[1];
+    }
+    else {
+      min = arr[1]; max = arr[0];
+    }
+    for (i = 2; i < number; i++) {
+      if (arr[i] < min) {
+        amount += min;      //arr<min
+        min = arr[i];
+      } else {
+        if (arr[i] > max) {
+          amount += max;  //arr>max
+          max = arr[i];
+        } else {
+          amount += arr[i]; //min<=arr<=max
+        }
+      }//if
+    }//for
+    avg = (double)amount / (number - 2);
+  }//if
+  return avg;
+}
---
+++ Seekers/project_p3.ino
@@ -0,0 +1,125 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+const char* ssid = "SB-IOT1";
+const char* password = "sb@iot11";
+String command1,command2;
+#define ORG "bhip5y"
+#define DEVICE_TYPE "Vamsi"
+#define DEVICE_ID "8500"
+#define TOKEN "8500913778"
+String command;
+char server[] = ORG ".messaging.internetofthings.ibmcloud.com";
+char topic[] = "iot-2/cmd/home/fmt/String";
+char authMethod[] = "use-token-auth";
+char token[] = TOKEN;
+char clientId[] = "d:" ORG ":" DEVICE_TYPE ":" DEVICE_ID;
+//Serial.println(clientID);
+
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
+#define SSD1306_LCDHEIGHT 64
+
+// OLED display TWI address
+#define OLED_ADDR   0x3C
+
+Adafruit_SSD1306 display(-1);
+
+#if (SSD1306_LCDHEIGHT != 64)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
+
+void callback(char* topic, byte* payload, unsigned int payloadLength);
+WiFiClient wifiClient;
+PubSubClient client(server, 1883, callback, wifiClient);
+void setup() {
+   display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR);
+  Serial.begin(115200);
+  Serial.println();
+  pinMode(D1,OUTPUT);
+  wifiConnect();
+  mqttConnect();
+}
+
+void loop() {
+  if (!client.loop()) {
+    mqttConnect();
+  }
+delay(100);
+}
+
+void wifiConnect() {
+  Serial.print("Connecting to "); Serial.print(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.print("nWiFi connected, IP address: "); Serial.println(WiFi.localIP());
+}
+
+void mqttConnect() {
+  if (!client.connected()) {
+    Serial.print("Reconnecting MQTT client to "); Serial.println(server);
+    while (!client.connect(clientId, authMethod, token)) {
+      Serial.print(".");
+      delay(500);
+    }
+    initManagedDevice();
+    Serial.println();
+  }
+}
+void initManagedDevice() {
+  if (client.subscribe(topic)) {
+    Serial.println("subscribe to cmd OK");
+  } else {
+    Serial.println("subscribe to cmd FAILED");
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int payloadLength) {
+  Serial.print("callback invoked for topic: "); Serial.println(topic);
+
+  for (int i = 0; i < payloadLength; i++) {
+    //Serial.println((char)payload[i]);
+    command += (char)payload[i];
+  }
+Serial.println(command);
+
+ 
+  
+command1=getValue(command,',',0);
+command2=getValue(command,',',1);
+if(command1=="3"){
+  display.clearDisplay();
+// display a line of text
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,10);
+  display.print(command2);
+  // update display with all of the above graphics
+  display.display();
+}
+command ="";
+command1 ="";
+command2="";
+}
+
+
+String getValue(String data, char separator, int index)
+{
+    int found = 0;
+    int strIndex[] = { 0, -1 };
+    int maxIndex = data.length() - 1;
+
+    for (int i = 0; i <= maxIndex && found <= index; i++) {
+        if (data.charAt(i) == separator || i == maxIndex) {
+            found++;
+            strIndex[0] = strIndex[1] + 1;
+            strIndex[1] = (i == maxIndex) ? i+1 : i;
+        }
+    }
+    return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
